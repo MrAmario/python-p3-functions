9d81218151019bd9ba061b2579ca20ac25884de8
Maxwell <maxwell.nganga@student.moringaschool.com>
2023-08-20 16:51:21 +0300

    completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    sum = (num1 + num2)
+    print(sum)
+    return sum
+add(1,2)
 
 def halve(number):
-    pass
+    half= number / 2
+    return half
+
+halve(4)
